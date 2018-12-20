8b471ef557d57c285caa299c32aad87fd6db3bfb
Bernard Zhao <bernard.zhao.us@gmail.com>
2018-12-19 17:08:18 -0800

    deploy

---
+++ js/chunk-2d0e5e97.d53d7988.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5e97"],{9703:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("span",[n._v("\n  Not a real route dum dum\n")])},o=[],l=t("2877"),a={},r=Object(l["a"])(a,u,o,!1,null,null,null);r.options.__file="NotFound.vue";e["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0e5e97.d53d7988.js.map
